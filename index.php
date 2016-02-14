4b3fc9c64092a53391e8d40821c84803af13c0a5
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-02-14 22:08:31 +0400

    Adding functionality to create files

--- index.php
+++ index.php
@@ -17,12 +17,33 @@ function getPathType($path) {
     return "Unknown";
 }
 
+function createItem($itemPath, $detail) {
+    $old = umask(0);
+
+    $type = $detail['-type'];
+
+    if ($type === 'dir') {
+        mkdir($itemPath, 0777, true);
+    } else if ( $type === 'file') {
+        $content = $detail['-content'];
+
+        $handle = fopen($itemPath,"wb");
+        fwrite($handle,$content);
+        fclose($handle);
+
+    } else if ($type === 'link') {
+
+    }
+
+    umask($old);
+}
+
 function directoryToArray( $path, $fullPath, &$parentArray ) {
 
     $parentArray[$path] = [];
  
     $parentArray[$path]['-name'] = $path;
-    $parentArray[$path]['-type'] = getPathType($fullPath);
+    $parentArray[$path]['-type'] = $itemType = getPathType($fullPath);
     $parentArray[$path]['-path'] = $fullPath;
     $parentArray[$path]['-size'] = filesize($fullPath);
     $parentArray[$path]['-mode'] = substr(sprintf('%o', fileperms($fullPath)), -4);
@@ -30,21 +51,23 @@ function directoryToArray( $path, $fullPath, &$parentArray ) {
     $parentArray[$path]['-last_modified'] = date('Y-m-d H:i:s', filemtime($fullPath));
     $parentArray[$path]['-group'] = posix_getgrgid(filegroup($fullPath));
 
-    // If it was a file, put the contents
-    if (is_file($fullPath)) {
-        $parentArray[$path]['content'] = file_get_contents($fullPath);
+    if($itemType === 'link') {
+        // Save the link detail
+    } else if ($itemType === 'file') {
+        // If it was a file, put the contents
+        $parentArray[$path]['-content'] = file_get_contents($fullPath);
+    } else if ($itemType === 'dir') {
+        // Recursively iterate the directory and find the inner contents
+        $handle = opendir($fullPath);
+
+        while($content = readdir($handle)) {
+            if ( $content == '.' || $content == '..') {
+                continue;
+            }
+
+            directoryToArray($content, $fullPath . '/' .$content, $parentArray[$path]);
+        };
     }
-
-    $handle = opendir($fullPath);
-
-    while($content = readdir($handle)) {
-
-        if ( $content == '.' || $content == '..') {
-            continue;
-        }
-
-        directoryToArray($content, $fullPath . '/' .$content, $parentArray[$path]);
-    };
 }
 
 function createDirectories($outputDir, $content) {
@@ -54,9 +77,7 @@ function createDirectories($outputDir, $content) {
     }
 
     if (!is_dir($outputDir)) {
-        $old = umask(0);
-        mkdir($outputDir, 0777);
-        umask($old);
+        createItem($outputDir, ['-type' => 'dir']);
     }
 
     foreach ($content as $label => $detail) {
@@ -68,17 +89,16 @@ function createDirectories($outputDir, $content) {
 
         $toCreate = $outputDir . '/' . $label;
 
-        if (!is_dir($toCreate)) {
+        if (!file_exists($toCreate)) {
             echo $toCreate . "<br>";
-
-            $old = umask(0);
-            mkdir($toCreate, 0777, true);
-            umask($old);
+            createItem($toCreate, $detail);
         }
 
-        createDirectories($toCreate, $detail);
+        if ($detail['-type'] == 'dir') {
+            createDirectories($toCreate, $detail);
+        }
     }
-
+    
 }
 
 // $array = [];
